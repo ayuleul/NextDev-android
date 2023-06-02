7bf46466aea0688c868af7f7a398658e97306698
Leul Ayalew <ayuleul1427@gmail.com>
2023-06-02 12:29:28 +0300

    add theme

--- app/src/main/java/com/leulayalew/nextdev/MainActivity.kt
+++ app/src/main/java/com/leulayalew/nextdev/MainActivity.kt
@@ -10,19 +10,18 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.leulayalew.nextdev.ui.theme.NextDevTheme
+import com.example.compose.NextDevTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             NextDevTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Greeting("Leul")
                 }
             }
         }
--- app/src/main/java/com/leulayalew/nextdev/ui/theme/Color.kt
+++ app/src/main/java/com/leulayalew/nextdev/ui/theme/Color.kt
@@ -1,8 +1,24 @@
-package com.leulayalew.nextdev.ui.theme
-
+package com.example.compose
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val md_theme_light_primary = Color(0xFF2F5DA8)
+val md_theme_light_onPrimary = Color(0xFFFFFFFF)
+val md_theme_light_secondary = Color(0xFF565E71)
+val md_theme_light_onSecondary = Color(0xFFFFFFFF)
+val md_theme_light_error = Color(0xFFBA1A1A)
+val md_theme_light_onError = Color(0xFFFFFFFF)
+val md_theme_light_background = Color(0xFFFEFBFF)
+val md_theme_light_onBackground = Color(0xFF1B1B1F)
+val md_theme_light_surface = Color(0xFFFEFBFF)
+val md_theme_light_onSurface = Color(0xFF1B1B1F)
+
+val md_theme_dark_primary = Color(0xFFACC7FF)
+val md_theme_dark_onPrimary = Color(0xFF002F67)
+val md_theme_dark_secondary = Color(0xFFBEC6DC)
+val md_theme_dark_onSecondary = Color(0xFF283041)
+val md_theme_dark_error = Color(0xFFFFB4AB)
+val md_theme_dark_onError = Color(0xFF690005)
+val md_theme_dark_background = Color(0xFF1B1B1F)
+val md_theme_dark_onBackground = Color(0xFFE3E2E6)
+val md_theme_dark_surface = Color(0xFF1B1B1F)
+val md_theme_dark_onSurface = Color(0xFFE3E2E6)
